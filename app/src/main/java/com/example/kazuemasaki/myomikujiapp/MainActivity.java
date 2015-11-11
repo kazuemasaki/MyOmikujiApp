9d0285e0d03c497d6c1f4af710057f78e6526283
kazuemasaki <kazuemasaki@gmail.com>
2015-11-11 13:58:53 +0900

    #05

--- app/src/main/java/com/example/kazuemasaki/myomikujiapp/MainActivity.java
+++ app/src/main/java/com/example/kazuemasaki/myomikujiapp/MainActivity.java
@@ -18,15 +18,20 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void getOmikuji(View view){
+    public void getOmikuji(View view) {
         //TextView取得
         TextView tv = (TextView) findViewById(R.id.myTextView);
+        String[] results = {
+                "大吉",
+                "吉",
+                "凶"
+        };
         //乱数の生成
         Random randomGenerator = new Random();
-        int num = randomGenerator.nextInt(3); // 0-2
+        int num = randomGenerator.nextInt(results.length); // 0-2
         //結果の表示
-        String result = Integer.toString(num);
-        tv.setText(result);
+        //String result = Integer.toString(num);
+        tv.setText(results[num]);
     }
 
 }
