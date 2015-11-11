858e7648dd73a5c85c603524271174d6ff052609
kazuemasaki <kazuemasaki@gmail.com>
2015-11-11 13:50:06 +0900

    #04

--- app/src/main/java/com/example/kazuemasaki/myomikujiapp/MainActivity.java
+++ app/src/main/java/com/example/kazuemasaki/myomikujiapp/MainActivity.java
@@ -4,6 +4,10 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -14,25 +18,15 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public void getOmikuji(View view){
+        //TextView取得
+        TextView tv = (TextView) findViewById(R.id.myTextView);
+        //乱数の生成
+        Random randomGenerator = new Random();
+        int num = randomGenerator.nextInt(3); // 0-2
+        //結果の表示
+        String result = Integer.toString(num);
+        tv.setText(result);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
